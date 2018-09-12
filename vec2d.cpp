74472842d929cc3cfe954921a4872b2ed32a6f1a
Joshua Levine <joshua.a.levine@gmail.com>
2018-09-12 15:48:24 -0700

    Comments and some prettification

--- vec2d.cpp
+++ vec2d.cpp
@@ -1,3 +1,45 @@
+///
+/// \file vec2d.cpp
+/// \brief 2D vector class
+/// \author Joshua A. Levine <josh@email.arizona.edu>
+/// \date 09/12/18
+///
+/// This code defines a simple vec2D class with some of the basic 
+/// operations for arithmetic and utility functions. 
+///
+
+
+/*
+ ***********************************************************************
+ 
+ Copyright (C) 2018, Joshua A. Levine
+ University of Arizona
+ 
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ ***********************************************************************
+ */
+
+
+
 #include "vec2d.h"
 
 #include <iostream>
@@ -19,6 +61,8 @@ vec2d::vec2d(const vec2d& v) {
 }
 
 
+//return *this is a typical assignment operator syntax, so that you can
+//chain multiple assignments, e.g. a = b = c
 vec2d& vec2d::operator=(const vec2d& rhs) {
   std::cout << "assignment operator" << std::endl;
   x = rhs.x;
@@ -47,6 +91,7 @@ vec2d vec2d::operator*(float s) {
 
 
 std::ostream& operator<<(std::ostream& os, const vec2d& obj) {
+  //print to stream in a pretty format
   os << "(" << obj.x << "," << obj.y << ")";
   
   return os;
--- vec2d.h
+++ vec2d.h
@@ -1,24 +1,69 @@
-#pragma once
+///
+/// \file vec2d.h
+/// \brief 2D vector class
+/// \author Joshua A. Levine <josh@email.arizona.edu>
+/// \date 09/12/18
+///
+/// This code declares a simple vec2D class with some of the basic 
+/// operations for arithmetic and utility functions. 
+///
 
-#include <iostream>
+
+/*
+ ***********************************************************************
+ 
+ Copyright (C) 2018, Joshua A. Levine
+ University of Arizona
+ 
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ ***********************************************************************
+ */
+
+
+
+#pragma once  // prevent double includes
+
+#include <iostream> // included for std::ostream
+
+
 
 class vec2d {
   public:
     vec2d();
     vec2d(float x_, float y_);
+    //copy constructor
     vec2d(const vec2d& v);
 
+    //assignment operator
     vec2d& operator=(const vec2d& rhs);
+    //arithmetic operations
     vec2d operator+(const vec2d& rhs);
     vec2d operator*(float s);
 
+    //friend functions can access private members, but are defined
+    //external to the class
     friend std::ostream& operator<<(std::ostream& os, const vec2d& obj);
-
-
 
   private:
     float x;
     float y;
-
-
 };
